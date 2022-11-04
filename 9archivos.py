a1f78e7d671732298dd595742f35c7be23d50cd6
Carlos Garcia <cgarcia@raloy.com.mx>
2022-11-04 17:40:17 -0600

    archivos

---
+++ 9archivos.py
@@ -0,0 +1,18 @@
+import numbers
+
+
+def read():
+    numbers = []
+    with open("./archivos/numbers.txt", "r", encoding="utf-8") as f
+
+def write():
+    pass
+
+def run():
+    read()
+
+
+
+
+if __name__ == '__main__':
+    run()
