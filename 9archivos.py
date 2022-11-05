6a781c2d140918ad4262a2ec90f6680ded6986c4
Carlos <tigrecarlos1@gmail.com>
2022-11-05 11:32:55 -0600

    manejo de archivos

--- 9archivos.py
+++ 9archivos.py
@@ -1,15 +1,26 @@
+from ast import For
 import numbers
 
 
 def read():
     numbers = []
-    with open("./archivos/numbers.txt", "r", encoding="utf-8") as f
+    with open("./archivos/numbers.txt", "r", encoding="utf-8") as f:
+        for line in f:
+            numbers.append(int(line))
+    print(numbers)
 
+# El atributo a es el metodo escritura sin sobrescritura (agrega)
+# El atributo w es escritura y sobrescritura (Edita lo que hay dentro)
 def write():
-    pass
+    names = ["CARLOS","PAPO"]
+    with open("./archivos/names.txt", "a", encoding="utf-8") as f:
+        for name in names:
+            f.write(name)
+            f.write("\n")
+
 
 def run():
-    read()
+    write()
 
 
 
